e8ac6c5b686c5a74fa9a0e8877dc3de45d510881
bigsaigon333 <likepeppermint@gmail.com>
2021-11-28 13:21:14 +0900

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+  ignorePatterns: ["*.js", "build"],
+  plugins: ["import", "@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: false,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+    project: ["./tsconfig.json"],
+  },
+  rules: {
+    "import/extensions": "off",
+    "no-restricted-syntax": "off",
+    "guard-for-in": "off",
+  },
+};
